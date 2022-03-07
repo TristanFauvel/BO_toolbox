fcd862af27e0cb8bb1b8cf7fa9b4d54a26a1d3d7
TristanFauvel <tristan.fauvel@hotmail.fr>
2022-03-07 18:54:26 +0100

    Debug in EI.

--- Binary_BO/Acquisition_funs_binary/EI_Tesch.m
+++ Binary_BO/Acquisition_funs_binary/EI_Tesch.m
@@ -1,4 +1,4 @@
-function [new_x, new_x_norm, ei] = EI_Tesch(theta, xtrain_norm, ctrain,model, post, approximation, optimization)
+function [new_x, new_x_norm, ei] = EI_Tesch(theta, xtrain_norm, ctrain,model, post, approximation, optim)
 %Expected improvement criterion by Tesch et al 2013
 if ~strcmp(func2str(model.link), 'normcdf')
     error('Function only implemented for a normcdf link')
@@ -14,7 +14,7 @@ ybest = norminv(mu_c_best);
 nsamps= 1e5;
 e = randn(1, nsamps);
 
- 
+
 [new_x_norm,ei] = optimize_AF(@(x)ExpImp(theta, xtrain_norm, ctrain, x, model, post,mu_c_best, ybest, e), model.lb_norm,  model.ub_norm, ncandidates, init_guess, options);
 new_x = new_x_norm.*(model.ub-model.lb) + model.lb;
 end
@@ -29,14 +29,13 @@ function [ei,deidx] = ExpImp(theta, xtrain_norm, ctrain, x, model, post, mu_c_be
 
 sigma_y = sqrt(sigma2_y);
 
-samples = mu_y + sigma_y*e; 
+samples = mu_y + sigma_y*e;
 samples(ybest>samples)=0;
 arg = model.link(samples) - mu_c_best;
 ei = mean(arg);
 
-[g, dgdx, dgdmu, dgdsigma] =  Gaussian(samples, mu_y, sigma_y);
+[g, dgdx, dgdmu, dgdsigma] =  Gaussian_fun(samples, mu_y, sigma_y);
 
 dsigmay_dx= dsigma2y_dx./(2*sigma_y);
 deidx = mean(arg.*(dgdmu.*dmuy_dx + dgdsigma.*dsigmay_dx)./g,2);
 end
-
